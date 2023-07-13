13d1613b8fb9a08b8d653734ed97ef3d62fab24f
M Ajis Pratama <85015184+Azisprtm0806@users.noreply.github.com>
2023-07-13 21:14:19 +0700

    public

--- App.js
+++ App.js
@@ -27,6 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(express.static("public"));
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://authentication-role.vercel.app"],
